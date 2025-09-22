f6eb793c38e4f6460d00f3ded83e24b65b5fae54
Hernan <hernan0066@gmail.com>
2025-09-22 20:16:31 -0300

    ...

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -18,4 +18,5 @@ export const auth0 = new Auth0Client({
     scope: process.env.AUTH0_SCOPE,
     audience: process.env.AUTH0_AUDIENCE,
   },
+  enableParallelTransactions: false, // Single-transaction mode
 });
